6a43e5fd6b99d79fa83f254d9a1ea8f6ff8178a0
sanket-walanj <code.sanky@gmail.com>
2022-03-30 23:44:10 +0530

    final commit

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-// let color = Math.random();
-// color = Math.random().toString(16);
-// color = Math.random().toString(16).substring(2, 8);
+// const rand = Math.floor(Math.random() * 10 + 1);
+// console.log(rand);
 
-// console.log(color);
-
-const hex = document.querySelector('.hex');
+const number = document.querySelector('.number');
 const btn = document.querySelector('.generate');
 
-const generateColor = () => {
-  const randomColor = Math.random().toString(16).substring(2, 8);
-  document.body.style.backgroundColor = '#' + randomColor;
-  hex.innerHTML = '#' + randomColor;
+const generateNumber = () => {
+  const randomNumber = Math.floor(Math.random() * 10 + 1);
+  number.innerHTML = randomNumber;
 };
 
-btn.addEventListener('click', generateColor);
-generateColor();
+btn.addEventListener('click', generateNumber);
+generateNumber();
